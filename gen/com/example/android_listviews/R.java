a448818862b66e0a860c07e05e1e45bdecbaa85d
Fedmich <fedmich@gmail.com>
2013-08-09 20:17:34 +0800

    downgrade to Android 2.2

--- gen/com/example/android_listviews/R.java
+++ gen/com/example/android_listviews/R.java
@@ -8,6 +8,9 @@
 package com.example.android_listviews;
 
 public final class R {
+    public static final class array {
+        public static final int colors=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -24,13 +27,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090001;
+        public static final int listView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -47,22 +51,11 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
